63a7f2a2ffe0dc4a6e385834f470d70b684ae435
Pedro Galdino <pedrogaldinob@gmail.com>
2026-05-20 15:06:33 -0300

    Alteração do Ususario e criação do repository
    Fiz a alteração do Usuário entity pra colocar a UserDetails que precisa pra fazer o security, e ja adicionei o repository pra poder adiantar as coisas

---
+++ turing/src/main/java/br/com/seuespacounb/turing/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.seuespacounb.turing.repository;
+
+import br.com.seuespacounb.turing.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
