9ffc813f8cbf2897018c270282dc1c2f220d606a
Samuel Carvalho <samuelcarvalho0201@outlook.com>
2026-05-22 19:52:35 -0300

    filtro security criado

---
+++ turing/src/main/java/br/com/seuespacounb/turing/config/SecurityFilter.java
@@ -0,0 +1,48 @@
+package br.com.seuespacounb.turing.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenConfig tokenConfig;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizedHeader = request.getHeader("Authorization");
+        if(Strings.isNotEmpty(authorizedHeader) && authorizedHeader.startsWith("Bearer")){
+            String token = authorizedHeader.substring("Bearer".length());
+            Optional<JWTUserData> optUser = tokenConfig.validateToken(token);
+            if (optUser.isPresent()) {
+                JWTUserData userData = optUser.get();
+
+                List<GrantedAuthority> authorities = List.of(
+                        new SimpleGrantedAuthority("ROLE_" + userData.role())
+                );
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userData, null, authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+}
