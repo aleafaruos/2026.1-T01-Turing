5fcd240bbe10aa5e01d24898ab5a479918c764b8
Samuel Carvalho <samuelcarvalho0201@outlook.com>
2026-05-22 19:57:44 -0300

    Melhoria nas roles

--- turing/src/main/java/br/com/seuespacounb/turing/config/SecurityConfig.java
+++ turing/src/main/java/br/com/seuespacounb/turing/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/usuarios/**").hasAnyRole("ADMIN", "CLIENT")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)//deixa publico somente requisições post para autenticação, restante é necessario esta logado
                 .build();
