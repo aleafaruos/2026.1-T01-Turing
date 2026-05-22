be8190b07335ad64dc3110dede498d8544617b85
Samuel Carvalho <samuelcarvalho0201@outlook.com>
2026-05-22 16:33:20 -0300

    Versao inicial do AuthController criado com register, login sera criado logo logo

---
+++ turing/src/main/java/br/com/seuespacounb/turing/controller/AuthController.java
@@ -0,0 +1,46 @@
+package br.com.seuespacounb.turing.controller;
+
+import br.com.seuespacounb.turing.dto.request.LoginRequest;
+import br.com.seuespacounb.turing.dto.request.RegisterUserRequest;
+import br.com.seuespacounb.turing.dto.response.LoginResponse;
+import br.com.seuespacounb.turing.dto.response.RegisterUserResponse;
+import br.com.seuespacounb.turing.entity.Usuario;
+import br.com.seuespacounb.turing.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+
+public class AuthController {
+    private final UsuarioRepository usuarioRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request){
+        return null;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterUserResponse> register(@Valid @RequestBody RegisterUserRequest request) {
+        Usuario novoUsuario = new Usuario();
+
+        novoUsuario.setName(request.name());
+        novoUsuario.setEmail(request.email());
+        novoUsuario.setCpf(request.cpf());
+        novoUsuario.setSenha(request.password());
+        novoUsuario.setTipoUsuario(request.tipoUsuario());
+
+        usuarioRepository.save(novoUsuario);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new RegisterUserResponse(novoUsuario.getName(), novoUsuario.getEmail()));
+    }
+}
