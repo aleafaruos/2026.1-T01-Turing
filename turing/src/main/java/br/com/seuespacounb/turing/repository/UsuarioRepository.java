835823703e726d7a9c17a1daa31cce5ec6a8210a
Samuel Carvalho <samuelcarvalho0201@outlook.com>
2026-05-22 15:59:54 -0300

    Corrige tipo de id do usuario(long nao integer) e adiciona findByEMail

--- turing/src/main/java/br/com/seuespacounb/turing/repository/UsuarioRepository.java
+++ turing/src/main/java/br/com/seuespacounb/turing/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import br.com.seuespacounb.turing.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+import java.util.Optional;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findUserByEmail(String email);
 }
