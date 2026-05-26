06766c9a47dd73e56fcb4fe5490504d113e0b09e
Pedro Galdino <pedrogaldinob@gmail.com>
2026-05-25 22:48:51 -0300

    Alterações no usuarioService e Controller
    Fiz todas as funcionalidades (que eu lembrei que precisavam) no usuarioService ou seja, as regras de negócio do sistema faltando agora so fazer a parte de autenticação no securityConfig, pra fazer as autenticações nas rotas e as suas diferenciações quanto ao tipo de usuário que pode acessar elas.
    Mexi também no controller ja fazendo todas as rotas.
    Agora so falta testar tudo e ver se ja ta tudo funcionando certinho, além de testar os possíveis erros também.

--- turing/src/main/java/br/com/seuespacounb/turing/controller/UsuarioController.java
+++ turing/src/main/java/br/com/seuespacounb/turing/controller/UsuarioController.java
@@ -1,9 +1,17 @@
 package br.com.seuespacounb.turing.controller;
 
+import br.com.seuespacounb.turing.dto.UsuarioDTO;
+import br.com.seuespacounb.turing.entity.Usuario;
+import br.com.seuespacounb.turing.exception.NotFoundException;
 import br.com.seuespacounb.turing.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -12,34 +20,68 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    //cadastra novo usuario
-    @PostMapping
-    public ResponseEntity<?> cadastrar() {
-        return null;
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void alterarDadosUsuario(
+            @AuthenticationPrincipal Usuario usuarioLogado,
+            @RequestBody UsuarioDTO usuarioDTO
+    ) throws NotFoundException {
+
+        usuarioService.alterarDadosProprioUsuario(usuarioLogado.getId(), usuarioDTO);
     }
 
-    //retorna o usuario por ‘id’
-    @GetMapping("/{id}")
-    public ResponseEntity<?> buscarPorId() {
-        return null; //temporario, apenas para compor o esqueleto antes da implementação real
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluirDadosProprioUsuario(
+            @AuthenticationPrincipal Usuario usuarioLogado
+    ) throws NotFoundException {
 
+        usuarioService.deletarDadosProprioUsuario(usuarioLogado.getId());
     }
 
-    //retorna lista de usuarios
+
+
+
+
+    @GetMapping("/adm")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Usuario> admGetUsuarios(
+            @AuthenticationPrincipal Usuario usuarioLogado
+    ) throws NotFoundException {
+
+        return usuarioService.getUsuarios(usuarioLogado.getId());
+    }
+
+    @PutMapping("/adm/{idUsuarioParaAlterar}")
+    @ResponseStatus(HttpStatus.OK)
+    public void admAlterarDadosUsuario(
+            @PathVariable Long idUsuarioParaAlterar,
+            @AuthenticationPrincipal Usuario usuarioLogado,
+            @RequestBody UsuarioDTO usuarioDTO
+    )throws NotFoundException {
+
+        usuarioService.admAlterarDadosUsuario(
+                usuarioLogado.getId(),
+                idUsuarioParaAlterar,
+                usuarioDTO
+        );
+    }
+
+    @DeleteMapping("/adm/{idUsuarioParaDeletar}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void admDeletarUsuario(
+            @PathVariable Long idUsuarioParaDeletar,
+            @AuthenticationPrincipal Usuario usuarioLogado
+    ) throws NotFoundException {
+        usuarioService.admDeletarUsuario(usuarioLogado.getId(), idUsuarioParaDeletar);
+    }
+
     @GetMapping
-    public ResponseEntity<?> listarUsuarios() {
-        return null;
-    }
-
-    //atualiza os dados do usuario
-    @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar() {
-        return null;
-    }
-
-    //remove o usuario
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> excluirUsuario() {
-        return null;
+    @ResponseStatus(HttpStatus.OK)
+    public Usuario admEncontrarUsuarioPorEmail(
+            @RequestParam String email,
+            @AuthenticationPrincipal Usuario usuarioLogado
+    ) throws NotFoundException {
+        return usuarioService.admEncontrarUsuarioPorEmail(usuarioLogado.getId(), email);
     }
 }
--- turing/src/main/java/br/com/seuespacounb/turing/service/UsuarioService.java
+++ turing/src/main/java/br/com/seuespacounb/turing/service/UsuarioService.java
@@ -1,18 +1,127 @@
 package br.com.seuespacounb.turing.service;
 
+import br.com.seuespacounb.turing.dto.UsuarioDTO;
+import br.com.seuespacounb.turing.entity.TipoUsuario;
+import br.com.seuespacounb.turing.entity.Usuario;
+import br.com.seuespacounb.turing.exception.NotFoundException;
+import br.com.seuespacounb.turing.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UsuarioService {
 
-    public void cadastrarUsuario(){}
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void buscarPorId(){}
+    public void alterarDadosProprioUsuario(Long idUsuarioLogado, UsuarioDTO usuarioDTO) throws NotFoundException {
 
-    public void listarUsuarios(){}
+        Usuario usuario = usuarioRepository.findById(idUsuarioLogado)
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
 
-    public void atualizarUsuario(){}
+        if (usuarioDTO.name() != null){
+            usuario.setName(usuarioDTO.name());
+        }
 
-    public void excluirUsuario(){}
+        if (usuarioDTO.email() != null){
+            usuario.setEmail(usuarioDTO.email());
+        }
 
+        if (usuarioDTO.cpf() != null){
+            usuario.setCpf(usuarioDTO.cpf());
+        }
+
+        if (usuarioDTO.senha() != null){
+            usuario.setSenha(passwordEncoder.encode(usuarioDTO.senha()));
+        }
+
+        usuarioRepository.save(usuario);
+    }
+
+    public void deletarDadosProprioUsuario(Long idUsuarioLogado) throws NotFoundException {
+        Usuario usuario = usuarioRepository.findById(idUsuarioLogado)
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
+
+        usuarioRepository.delete(usuario);
+    }
+
+
+
+
+
+
+    public List<Usuario> getUsuarios(Long idAdm) throws NotFoundException {
+        Usuario usuarioAdm = usuarioRepository.findById(idAdm)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        if (usuarioAdm.getTipoUsuario() != TipoUsuario.ADMIN) {
+            throw new AccessDeniedException("Usuário não é um administrador");
+        }
+
+        return usuarioRepository.findAll();
+    }
+
+    public void admAlterarDadosUsuario(Long idAdm, Long idUsuarioPraAlterar, UsuarioDTO usuarioDTO) throws NotFoundException {
+        Usuario usuarioAdm = usuarioRepository.findById(idAdm)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        Usuario usuarioCliente = usuarioRepository.findById(idUsuarioPraAlterar)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        if (usuarioAdm.getTipoUsuario() != TipoUsuario.ADMIN) {
+            throw new AccessDeniedException("Usuário não é um administrador");
+        }
+
+        if (usuarioDTO.name() != null){
+            usuarioCliente.setName(usuarioDTO.name());
+        }
+
+        if (usuarioDTO.email() != null){
+            usuarioCliente.setEmail(usuarioDTO.email());
+        }
+
+        if (usuarioDTO.cpf() != null){
+            usuarioCliente.setCpf(usuarioDTO.cpf());
+        }
+
+        if (usuarioDTO.tipoUsuario() != null) {
+            usuarioCliente.setTipoUsuario(usuarioDTO.tipoUsuario());
+        }
+
+        usuarioRepository.save(usuarioCliente);
+    }
+
+    public void admDeletarUsuario(Long idAdm, Long idUsuarioDeletado) throws NotFoundException {
+        Usuario usuarioAdm = usuarioRepository.findById(idAdm)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        Usuario usuarioCliente = usuarioRepository.findById(idUsuarioDeletado)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        if (usuarioAdm.getTipoUsuario() != TipoUsuario.ADMIN) {
+            throw new AccessDeniedException("Usuário não é um administrador");
+        }
+
+        usuarioRepository.delete(usuarioCliente);
+    }
+
+    public Usuario admEncontrarUsuarioPorEmail(Long idAdm, String email) throws NotFoundException {
+
+        Usuario usuarioAdm = usuarioRepository.findById(idAdm)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        if (usuarioAdm.getTipoUsuario() != TipoUsuario.ADMIN) {
+            throw new AccessDeniedException("Usuário não é um administrador");
+        }
+
+        Usuario usuarioCliente = usuarioRepository.findUserByEmail(email)
+                .orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+
+        return usuarioCliente;
+    }
 }
