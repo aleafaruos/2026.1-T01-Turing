20898e9cf7ea1b581bce7e6524a49d86efe58c69
Veloxz.Alan <alanfariasb01@gmail.com>
2026-05-30 17:35:25 -0300

    feat: implementa MapStruct e refatora SalaService e SalaController para usar DTOs

---
+++ turing/src/main/java/br/com/seuespacounb/turing/mapstruct/SalaMapper.java
@@ -0,0 +1,16 @@
+package br.com.seuespacounb.turing.mapstruct;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import br.com.seuespacounb.turing.dto.SalaRequestDTO;
+import br.com.seuespacounb.turing.dto.SalaResponseDTO;
+import br.com.seuespacounb.turing.entity.Sala;
+
+@Mapper(componentModel = "spring", uses = {HorarioSalaMapper.class})
+public interface SalaMapper {
+    Sala toEntity(SalaRequestDTO dto);
+    SalaResponseDTO toResponseDTO(Sala entity);
+    List<SalaResponseDTO> toListResponseDTO(List<Sala> entities);
+}
