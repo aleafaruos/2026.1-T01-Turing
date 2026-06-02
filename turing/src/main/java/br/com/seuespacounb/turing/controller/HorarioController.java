09c2df68613063c88d0df907d294380e98c6be6c
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-06-02 15:06:41 -0300

    :sparkles: feat: Na HorarioController em salvar adiciona @Validated(OnCreate.class) e em excluir adiciona exclusão por período, dia e hora com NotFoundException

--- turing/src/main/java/br/com/seuespacounb/turing/controller/HorarioController.java
+++ turing/src/main/java/br/com/seuespacounb/turing/controller/HorarioController.java
@@ -3,12 +3,18 @@ package br.com.seuespacounb.turing.controller;
 import br.com.seuespacounb.turing.dto.HorarioSalaRequestDTO;
 import br.com.seuespacounb.turing.dto.HorarioSalaResponseDTO;
 import br.com.seuespacounb.turing.exception.ConflictException;
+import br.com.seuespacounb.turing.exception.NotFoundException;
 import br.com.seuespacounb.turing.service.HorarioSalaService;
+import br.com.seuespacounb.turing.validation.groups.OnCreate;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -26,14 +32,14 @@ public class HorarioController {
     }
 
     @PostMapping
-    public ResponseEntity<HorarioSalaResponseDTO> salvar(@RequestBody HorarioSalaRequestDTO horarioSalaRequestDTO) throws ConflictException {
+    public ResponseEntity<HorarioSalaResponseDTO> salvar(@Validated(OnCreate.class) @RequestBody @Valid HorarioSalaRequestDTO horarioSalaRequestDTO) throws ConflictException, NotFoundException{
         HorarioSalaResponseDTO horarioSalvo =  horarioSalaService.salvarHorario(horarioSalaRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(horarioSalvo);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> excluir(@PathVariable Long id) {
-        horarioSalaService.excluirHorarioPorSala(id);
+    @DeleteMapping("/{id}/{inicioPeriodo}/{diaSemana}/{inicioHora}")
+    public ResponseEntity<String> excluir(@PathVariable Long id, @PathVariable LocalDate inicioPeriodo, @PathVariable DayOfWeek diaSemana, @PathVariable LocalTime inicioHora) throws NotFoundException {
+        horarioSalaService.excluirHorarioPorSala(id, inicioPeriodo, diaSemana, inicioHora);
         return ResponseEntity.ok("Horário removido");
     }
 }
